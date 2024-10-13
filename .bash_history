c4168ed5565aadff42fc8f4635377fd56948c6f4
João Paulo Rossi <j.p.rossi@hotmail.com>
2024-10-13 18:17:58 +0000

    Primeiro commit - Adicionando o projeto de cálculo de IMC

---
+++ .bash_history
@@ -0,0 +1,7 @@
+/bin/python /home/rossi_rockero/calculadora_imc.py
+/bin/python /home/rossi_rockero/calculadora_imc.py
+cd
+dir
+python3 calculadora_imc.py
+/bin/python /home/rossi_rockero/calculadora_imc.py
+git init
---
+++ .codeoss/data/User/History/-375ac8c6/sxrh.py
@@ -0,0 +1,27 @@
+def calcular_imc(peso, altura):
+    imc = peso / (altura ** 2)
+    return imc
+
+def main():
+    try:
+        peso = float(input("Digite seu peso em kg: "))
+        altura = float(input("Digite sua altura em metros (utilize ponto ao inves de virgula)): "))
+        
+        imc = calcular_imc(peso, altura)
+        
+        print(f"Seu IMC é: {imc:.2f}")
+        
+        if imc < 18.5:
+            print("Você está abaixo do peso.")
+        elif 18.5 <= imc < 24.9:
+            print("Você está com peso normal.")
+        elif 25 <= imc < 29.9:
+            print("Você está com sobrepeso.")
+        else:
+            print("Você está com obesidade.")
+    
+    except ValueError:
+        print("Insira valores numéricos válidos para peso e altura.")
+
+if __name__ == "__main__":
+    main()
